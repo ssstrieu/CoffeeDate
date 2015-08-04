36c80d7b598c53a772a3266de55dfb7f647cac84
ssstrieu <samanthastrieu@gmail.com>
2015-08-03 19:46:35 -0700

    ready to deploy v1

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,12 @@
 $( document ).ready(function(){
   console.log('=======document is now ready!!!!!======');
 
+ $( ".bizCategory" ).on( "click", function() {
+    console.log('click event');
+    // $(".item-container li").removeClass( "active" );
+    $( this ).addClass( "active" );
+  });
+
   function initialize() {
   var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
   var mapOptions = {
@@ -14,6 +20,8 @@ $( document ).ready(function(){
       map: map,
       title: 'Hello World!'
   });
+
+   
   }
 
   var result=google.maps.event.addDomListener(window, 'load', initialize);
